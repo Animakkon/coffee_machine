10dc5e92f8798aab7fb3ebce4b023b50454e5b4b
Animakkon <lada.dunskaya@mail.ru>
2022-08-31 15:29:52 +0300

    add: buttons

--- lib/pages/addResources.dart
+++ lib/pages/addResources.dart
@@ -1,5 +1,7 @@
 import 'package:coffee_machine/classes/Machine.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class AddResources extends StatefulWidget {
   const AddResources({Key? key}) : super(key: key);
@@ -59,114 +61,151 @@ class _AddResource extends State<AddResources> {
                   ),
                   Text('Cash: ${machine.getResources().getCash()}',
                       style:
-                      const TextStyle(fontSize: 25, color: Colors.black54)
-                  ),
+                          const TextStyle(fontSize: 25, color: Colors.black54)),
                 ],
               ),
             ),
           ),
         ),
         Container(
-          height: 250,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
           width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 5,
-                blurRadius: 15,
-                offset: Offset.zero,
-              )
-            ]
-          ),
-          //todo: form controls
-            child: Container(
-              alignment: Alignment.center,
-              child: Row(
+          decoration: BoxDecoration(color: Colors.white, boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 15,
+              offset: Offset.zero,
+            )
+          ]),
+          child: Column(
+            children: <Widget>[
+              // todo: add changed image
+              // Row(
+              //   children: <Widget>[
+              //     if (_milk.currentState!.validate()) Image(
+              //       image: const AssetImage("assets/icons/milk-correct.png"),
+              //       width: 50.0,
+              //       height: 50.0,
+              //     ) else Image(
+              //       image: const AssetImage("assets/icons/milk.png"),
+              //       width: 50.0,
+              //       height: 50.0,
+              //     ),
+              //     Expanded(
+              //       child: TextFormField(
+              //         key: _milk,
+              //         controller: milkController,
+              //         decoration: const InputDecoration(hintText: 'put milk'),
+              //         validator: (String? value) {
+              //           if (value == null || value.isEmpty) {
+              //             return 'please enter milk';
+              //           }
+              //           try {
+              //             var milk = int.parse(value);
+              //           } catch (e) {
+              //             return 'you put strange resource!!!';
+              //           }
+              //           return null;
+              //         },
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              TextFormField(
+                key: _milk,
+                controller: milkController,
+                decoration: const InputDecoration(hintText: 'put milk'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'please enter milk';
+                  }
+                  try {
+                    var milk = int.parse(value);
+                  } catch (e) {
+                    return 'you put strange resource!!!';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                key: _water,
+                controller: waterController,
+                decoration: const InputDecoration(hintText: 'put water'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'please enter water';
+                  }
+                  try {
+                    var water = int.parse(value);
+                  } catch (e) {
+                    return 'you put strange resource!!!';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                key: _beans,
+                controller: beansController,
+                decoration: const InputDecoration(hintText: 'put beans'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'please enter beans';
+                  }
+                  try {
+                    var beans = int.parse(value);
+                  } catch (e) {
+                    return 'you put strange resource!!!';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                key: _cash,
+                controller: cashController,
+                decoration: const InputDecoration(hintText: 'put cash'),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'please enter cash';
+                  }
+                  try {
+                    var cash = int.parse(value);
+                  } catch (e) {
+                    return 'you put strange cash!!!';
+                  }
+                  return null;
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // todo: сделать генерацию полей из массива?
-                  Flexible(
-                    flex: 2,
-                    child: Container(
-                      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            key: _milk,
-                            controller: milkController,
-                            decoration: const InputDecoration(
-                                hintText: 'put milk'),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'please enter milk';
-                              }
-                              try {
-                                var milk = int.parse(value);
-                              } catch (e) {
-                                return 'you put strange resource!!!';
-                              }
-                              return null;
-                            },
-                          ),
-                          TextFormField(
-                            key: _water,
-                            controller: waterController,
-                            decoration: const InputDecoration(
-                                hintText: 'put water'),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'please enter water';
-                              }
-                              try {
-                                var water = int.parse(value);
-                              } catch (e) {
-                                return 'you put strange resource!!!';
-                              }
-                              return null;
-                            },
-                          ),
-                          TextFormField(
-                            key: _beans,
-                            controller: beansController,
-                            decoration: const InputDecoration(
-                                hintText: 'put beans'),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'please enter beans';
-                              }
-                              try {
-                                var beans = int.parse(value);
-                              } catch (e) {
-                                return 'you put strange resource!!!';
-                              }
-                              return null;
-                            },
-                          ),
-                          TextFormField(
-                            key: _cash,
-                            controller: cashController,
-                            decoration: const InputDecoration(
-                                hintText: 'put cash'),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                return 'please enter cash';
-                              }
-                              try {
-                                var cash = int.parse(value);
-                              } catch (e) {
-                                return 'you put strange cash!!!';
-                              }
-                              return null;
-                            },
-                          )
-                        ],
-                      ),
-                    ),
-                  )
+                  Container(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    decoration: BoxDecoration(
+                        color: Colors.lightGreen,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: IconButton(
+                        icon: const Icon(Icons.add),
+                        // todo: высвечивающаяся подсказка
+                        // todo: обавить метод добавления ресурсов
+                        onPressed: () => setState(() {})),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 5, right: 5),
+                    decoration: BoxDecoration(
+                        color: Colors.redAccent,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: IconButton(
+                        icon: const Icon(Icons.add),
+                        // todo: высвечивающаяся подсказка
+                        // todo: обавить метод бнуления счета
+                        onPressed: () => setState(() {})),
+                  ),
                 ],
               ),
-            ),
+            ],
+          ),
         ),
       ],
     );
